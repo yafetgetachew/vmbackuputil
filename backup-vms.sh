19b1a45cf4da04a611d76318bcd130fc3796fff4
Yafet Getachew <yafetgetachew@camara.ie>
2018-08-15 12:37:20 +0300

    Improved shutdown, added loopable assoc arrays and added ova build feature

--- backup-vms.sh
+++ backup-vms.sh
@@ -7,12 +7,58 @@
 #
 
 # create array with names of virtual machines
+echo
+echo "Camara Virtual Machine Backup Utility 0.1"
+echo
+
+declare -A virtualmachines
+
+virtualmachines=(
+	[bitnami-edx_1]="172.16.10.249"
+	)
+
+users=(
+	[bitnami-edx_1]="bitnami"
+	)
+
+passwords=(
+	[bitnami-edx_1]="bitnami"
+	)
+
+#check if sshpass is installed and install it if not
+
+if ! (dpkg-query -l sshpass) > /dev/null; then
+   echo -e "sshpass not installed, installing (sudo might be required)... "
+   sudo apt install sshpass
+fi
 
 
-declare -a virtual_machines=("bitnami-edx_1")
 
 # loop through virtual machines and shut them down
-for i in "${virtual_machines[@]}"
+for i in "${!virtualmachines[@]}"
 do
-	sshpass -p "bitnami" ssh -o StrictHostKeyChecking=no bitnami@IP poweroff
+
+	echo "shutting down $i through ip ${virtualmachines[$i]}"
+	sshpass -p ${passwords[$i]} ssh -o StrictHostKeyChecking=no ${users[$i]}@${virtualmachines[$i]} sudo poweroff
+
+	echo "Waiting for machine $i to poweroff..."
+
+	until $(VBoxManage showvminfo --machinereadable $i | grep -q ^VMState=.poweroff.)
+
+	do
+	  sleep 1
+	  echo "poweroff state not reached for $i"
+	done
+
+	echo "poweroff of $i successful, building ova"
+	echo
+
+	# build .ova in home directory to then transport to medemer
+	VBoxManage export $i -o $i.ova
+
+
 done
+
+echo
+
+
