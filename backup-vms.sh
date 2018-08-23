dc94d8a9f8ff6ad948b9059f5aa773d04d7b5ea7
Yafet Getachew <yafetgetachew@camara.ie>
2018-08-23 11:07:28 +0300

    added gitignore, added more machines to backup, changed filenames

--- backup-vms.sh
+++ backup-vms.sh
@@ -6,7 +6,7 @@
 #
 #
 
-# create array with names of virtual machines
+# create array with names, ips, usernames and passwords of virtual machines
 echo
 echo "Camara Virtual Machine Backup Utility 0.1"
 echo
@@ -15,18 +15,27 @@ declare -A virtualmachines
 
 virtualmachines=(
 	[bitnami-edx]="172.16.10.249"
+	[ubuntu_nms]="172.16.10."
+	[ubuntu_kolibri_new]="172.16.10.15"
+
 	)
 
 users=(
 	[bitnami-edx]="bitnami"
+	[ubuntu_nms]="camaraadmin"
+	[ubuntu_kolibri_new]="camaraadmin"
+
 	)
 
 passwords=(
 	[bitnami-edx]="bitnami"
+	[ubuntu_nms]="Camara1604!"
+	[ubuntu_kolibri_new]="Camara1604!"
+
 	)
 
 #make a directory 
-mkdir -p ~/backup 
+mkdir -p backup-vms
 
 #check if sshpass is installed and install it if not
 if ! (dpkg-query -l sshpass) > /dev/null; then
@@ -56,13 +65,13 @@ do
 	echo
 
 	# build .ova in home directory to then transport to medemer
-	VBoxManage export $i -o ~/backup/$i-$(date +"%m-%d-%y").ova
+	VBoxManage export $i -o backup-vms/$i-$(date +"%m-%d-%y").ova
 
 	# Copy backed up file to remote backup folder
-	rsync -e 'ssh -p 22' -avzp ~/backup medemer@172.16.10.8:~/backup
+	rsync -e 'ssh -p 22' -avzp backups medemer@172.16.10.8:~/backup-vms
+	rm -rf backup-vms/*
 
-
-
+	echo "SUCCESSFUL BACKUP OPERATION"
 done
 
 echo
